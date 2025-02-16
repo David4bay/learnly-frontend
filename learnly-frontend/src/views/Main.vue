332460b5d570b25f5ccbe2d5cc78e83f20a0cfcc
David Bayode <david4bay@gmail.com>
2025-02-16 10:36:59 +0100

    building reusable lesson and quiz links

---
+++ learnly-frontend/src/views/Main.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Lessons from '@/components/Lessons.vue'
+import Quizes from '@/components/Quizes.vue'
+import QuizSlot from '@/components/QuizSlot.vue'
+</script>
+<style scoped>
+    .main__content {
+        display: flex;
+        flex-direction: row;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+<template>
+    <main class="main__content">
+        <QuizSlot quizType="lessons"/>
+        <QuizSlot quizType="quizes" />
+        <Lessons /> 
+        <Quizes />
+    </main>
+</template>
